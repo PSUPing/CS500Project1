36f70d42d9a3f65667c707ef976e737fa3c60bcb
PSUPing <matthew.ping@psualum.com>
2014-07-30 09:03:12 -0400

    Initial Commit

---
+++ Code/User.java
@@ -0,0 +1,80 @@
+import java.sql.DatabaseMetaData;
+
+/**
+ *
+ * User Class
+ *
+ */
+public class User {
+
+    private String userID;
+    private String userPwd;
+    private java.sql.Date userDOB;
+    private java.sql.Date userJoined;
+
+    /********** Constructors **********/
+
+    public User(String uid) {
+        userID = uid;
+    }
+
+    public User(String uid, String pwd, java.sql.Date dob, java.sql.Date joined) {
+        userID = uid;
+        userPwd = pwd;
+        userDob = dob;
+        userJoined = joined;
+    }
+
+    /********** Getters **********/
+
+    public String getUID() {
+        return userID;
+    }
+
+    public String getPassword() {
+        return userPwd;
+    }
+
+    public java.sql.Date getDOB() {
+        return userDob;
+    }
+
+    public java.sql.Date getDateJoined() {
+        return userJoined;
+    }
+
+    /********** Setters **********/
+
+    public void setUID(String uid) {
+        userID = uid;
+    }
+
+    public void setPassword(String pwd) {
+        userPwd = pwd;
+    }
+
+    public void setDOB(java.sql.Date dob) {
+        userDob = dob;
+    }
+
+    public void setBio(java.sql.Date joined) {
+        userJoined = joined;
+    }
+
+    /********** Output Methods **********/
+
+    public String toString() {
+        return "(UID: " + userID + ") Password: " + userPwd + " DOB: " + userDob + " Date Joined: " + userJoined;
+    }
+
+    public String toViewHTML() {
+        return "<tr><td>" + userID + "</td><td>" + userPwd + "</td><td>" + userDob + "</td><td>" + userJoined + "</td><td></tr>";
+    }
+
+    public String toEditHTML() {
+        return "<tr><td><input type=\"text\" name=\"uID\" />" + userID +
+               "</td><td><input type=\"password\" name=\"uPwd\" />" + userPwd +
+               "</td><td><input type=\"text\" name=\"uDob\" />" + userDob +
+               "</td><td><input type=\"text\" name=\"uJoined\" />" + userJoined + "</td><td></tr>";
+    }
+}
