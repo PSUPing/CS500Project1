08790e00c87948a247432522e020e218d401a2b3
PSUPing <matthew.ping@psualum.com>
2014-07-30 08:46:20 -0400

    Added Create Tables
    Just doing this because I'm on my phone.

--- SQL/Registrar.sql
+++ SQL/Registrar.sql
@@ -53,3 +53,85 @@ insert into Enrollment(cid, did, sid, term, grade) values (210, 200, 1, 'Spring 
 insert into Enrollment(cid, did, sid, term, grade) values (210, 200, 2, 'Spring 2014', 3);
 insert into Enrollment(cid, did, sid, term, grade) values (210, 200, 3, 'Spring 2014', 4);
 insert into Enrollment(cid, did, sid, term) values (210, 200, 4, 'Spring 2014');
+
+create table Users(
+	UID number primary key,
+	pwd varchar(128),
+	dob date,
+	date_joined date
+);
+
+create table Actors(
+	AID number primary key,
+	name varchar(128),
+	dob date,
+	bio varchar(600)
+);
+
+create table Titles(
+	MID number primary key,
+	name varchar(128),
+	genre varchar(128),
+	year number,
+	synopsis varchar(600),
+	title_type varchar(50)
+);
+
+create table Actors_Role_In(
+	AID number,
+	MID number,
+	role varchar(128),
+	primary key(AID, MID),
+	foreign key (MID) references Titles(MID),
+	foreign key (AID) references Actors(AID)
+);
+
+create table Ratings(
+	RatID number primary key,
+	AID number,
+	MID number,
+	UID number,
+	score number,
+	foreign key (MID) references Titles(MID),
+	foreign key (AID) references Actors(AID)
+	foreign key (UID) references Users(UID)
+);
+
+create table Reviews(
+	RevID number primary key,
+	rating number,
+	MID number,
+	rs varchar(128), --review source
+	rt varchar(500), --review text
+	foreign key (MID) references Titles(MID)
+);
+
+create table Quotes(
+	QID number primary key,
+	qt varchar(500), --quote text
+	AID number,
+	foreign key (AID) references Actors(AID)
+);
+
+create table Trivia(
+	TID number primary key,
+	AID number,
+	trivia_text varchar(600),
+	foreign key (AID) references Actors(AID)
+);
+
+create table Awards(
+	AwID number primary key,
+	AID number,
+	nomination_date date,
+	award_date date,
+	foreign key (AID) references Actors(AID)
+);
+
+create table News(
+	NID number primary key,
+	AID number,
+	news_source varchar(128),
+	news_url varchar(50),
+	foreign key (AID) references Actors(AID)
+);
